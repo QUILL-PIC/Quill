2bd52d5053a418bc948cac2a1a170400dc24b4fa
Dmitry Serebryakov <dmserebr@gmail.com>
2016-02-18 14:35:29 +0400

    Quill: auto creation of data folder + copying of config file into it

--- quill3d/run.sh
+++ quill3d/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Create data folder if not exist; copy config file into it
+folder=`./parse.sh "$1" | grep -A2 data_folder | tail -n1`
+echo "Data folder: $folder"
+sleep 1.5  # so the user has time to see the data folder
+
+mkdir -p $folder
+cp ../quill3d-conf/quill.conf"$1" $folder/quill.conf"$1"
+
 ./parse.sh "$1" | ./quill
