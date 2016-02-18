fb7d040dbfa45ad48b35093073f311c714bf62eb
Dmitry Serebryakov <dmserebr@gmail.com>
2016-02-19 00:59:32 +0400

    Quill: added logging with timestamps

--- quill3d/run.sh
+++ quill3d/run.sh
@@ -8,4 +8,8 @@ sleep 1.5  # so the user has time to see the data folder
 mkdir -p $folder
 cp ../quill3d-conf/quill.conf"$1" $folder/quill.conf"$1"
 
-./parse.sh "$1" | ./quill
+# 1. Parsing config file (parse.sh)
+# 2. Running quill with input from step 1
+# 3. Adding timestamps to the Quill's console output
+# 4. Duplicating output to a log file (quill_log.txt)
+stdbuf -o 0 ./parse.sh "$1" | ./quill 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S\t"), $0; fflush(); }' | tee $folder/quill_log.txt
