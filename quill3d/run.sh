0caa06fd10ee42e4f2c52a181a94221a264d422d
Evgeny Nerush <nerush@appl.sci-nnov.ru>
2016-04-15 12:20:35 +0300

    run.sh bugfix: now Segfault message is not lost

--- quill3d/run.sh
+++ quill3d/run.sh
@@ -12,4 +12,4 @@ cp ../quill3d-conf/quill.conf"$1" $folder/quill.conf"$1"
 # 2. Running quill with input from step 1
 # 3. Adding timestamps to the Quill's console output
 # 4. Duplicating output to a log file (quill_log.txt)
-stdbuf -o 0 ./parse.sh "$1" | ./quill 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S\t"), $0; fflush(); }' | tee $folder/quill_log.txt
+stdbuf -o 0 ./parse.sh "$1" | { ./quill; } 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S\t"), $0; fflush(); }' | tee $folder/quill_log.txt
