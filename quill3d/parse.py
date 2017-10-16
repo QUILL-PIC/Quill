5b041bcc6bded7ff0c63232d9eac2a4d5eaf923a
Anton Golovanov <agolovanov256@gmail.com>
2017-10-16 15:11:21 +0300

    Move to a parser from the master branch

--- quill3d/parse.py
+++ quill3d/parse.py
@@ -1,46 +1,49 @@
-import os
+#!/usr/bin/env python
+import sys
 import re
 
-file_path = os.path.dirname(os.path.realpath(__file__))
-file_path +="/parse_temp"
+prefix = '../quill3d-conf/quill.conf'
 
-def parse_fun(file_path):
+# regex for numbers
+number_regex = r'([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'
+# regex for array of numbers in format '[1;2;3]'
+array_regex = r'(\[' + number_regex + r'(;' + number_regex + r')*\])'
+# value can be either a number or an array
+value_regex = '(' + number_regex + '|' + array_regex + ')'
+# dimension can contain word symbols or percent sign, as well as power e.g. "^{-3}"
+dimension_regex = r'(\w+|%)(\^{?[+-]?\d+}?)?'
+# value with the dimension are separated by space
+value_dimension_regex = value_regex + '\s+' + dimension_regex
+# text only values can contain word symbols, digits, dots, slashes, ampersands and hyphens
+text_only_regex = r'([\w\d&-./]+)'
 
-	export_file=open(file_path,"r")
-	temp_file=open("temp","w")
+conf = prefix + sys.argv[1]
+with open(conf, "r") as f:
+    for l in f:
+        if '#' in l:
+            l = l[:l.find('#')]
+        l = l.strip()
+        if l == '':
+            continue
+        key_value = l.split('=')
+        if len(key_value) != 2:
+            raise Exception('Line [%s] is invalid' % l)
 
-	for line in export_file:
-		n=line.find('#') #delete comments
-		line_new=line[:n]
-		result1=re.split(r'=',line_new) #first parse line
-		if result1[0]!='':
-			st=result1[1]
-			result2=re.split(r' ',st) #parse expression after "="
-			print(result1[0])
-			if result2[1]!='' and result2[1].isalpha(): #chech element is string or number
-				print('#')
-				print(result2[1])
-				print('$')
-			else:
-				print(result2[1])
-				if len(result2)>2:
-					if result2[2]!='':
-						print(result2[2])
-						print('$')
-					else:
-						print('#')
-						print('$')
-				else:
-					print('#')
-					print('$')
-
-	print('$')
-
-	export_file.close()
-	temp_file.close()
-
-def main():
-	parse_fun(file_path)
-
-if __name__ == "__main__":
-    main()
+        key = key_value[0].strip()
+        value = key_value[1].strip()
+        print(key)
+        if re.match('^' + value_regex + '$', value):
+            print(value)
+            print('#')
+        elif re.match('^' + text_only_regex + '$', value):
+            print('#')
+            print(value)
+        elif re.match('^' + value_dimension_regex + '$', value):
+            v = re.findall(value_regex, value)[0][0]
+            print(v)
+            d = value[len(v):].strip()
+            print(d)
+        else:
+            raise Exception('Syntax of value [%s] of key [%s] is incorrect' % (value, key))
+        print('$')
+    print('$')
--- quill3d/parse.sh
+++ quill3d/parse.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cat ../quill3d-conf/quill.conf"$1" | sed -e 's/#\(.*\)$//;s/\s//g;/^$/d;s/$/\n$/' | sed -n 's/\$/&/p;s/\[*\]/&\n/p;t;s/=[+-]\?[0-9]*\(.[0-9]\+\)\?\(e[+-]\?[0-9]\+\)\?/&\n/p' | sed -e 's/^$/#/'| sed -e 's/=[0-9]/\n&/;s/=//;/^$/d' | sed -e 's/^$/#/;$a\$' |sed -e 's/\[/\n&/' | sed -e 's/[+-]/\n&/' >> test
-python ./edit_sed_cout.py
-cat temp
-rm temp
+cat ../quill3d-conf/quill.conf"$1" | sed -e 's/#\(.*\)$//;s/\s//g;/^$/d;s/$/\n$/;s/=[+-]\?[0-9]*\(.[0-9]\+\)\?\(e[+-]\?[0-9]\+\)\?/&\n/;s/=/\n/' | sed -e 's/^$/#/;$a\$'
 
 # Содержание строк после '#' удаляется
 # Пробелы удаляются
@@ -24,11 +21,3 @@ rm temp
 # $
 # Если [значение] и/или [размерность] не указаны, то вместо них
 # пишется символ "#"
-
-#This script parses input data stream for Quill's initialization.
-#First part deletes all string after #, spacing, empty strings.
-#Sed with key '-n' searches strings with $ and prints its, then finds expression in [] or +1.2e-5 (prints one of them)
-#Next in place empty stirng paste #
-#Thereafter = inserts new line, after = and empty line deletes
-#Append $ to the end stream
-#Insetrs new line after [
