d7122b3b43eb870e426bb3614aff6bf87e467a6f
Dmitry Serebryakov <dmserebr@gmail.com>
2018-11-30 20:18:05 +0300

    Generation of horizontal brush

--- quill3d/generate_brush.py
+++ quill3d/generate_brush.py
@@ -4,6 +4,8 @@
 # Insert into config template (for plain config file, just replace $ values with actual numbers)
 # exec{import generate_brush}
 # eval{generate_brush.generate($x0film, $dxbrush, $y0brush, $dybrush, $brushperiod, $y1film, $xnpic_brush, $ynpic_brush, $znpic_brush)}
+# Example with horizontal brush:
+# eval{generate_brush.generate_horizontal(2, $dxbrush, 2.5, 2.5 + $dybrush, $brushperiod, 13, $xnpic_brush, $ynpic_brush, $znpic_brush)}
 
 def generate(x0film, dxbrush, y0brush, dybrush, brushperiod, ymax, xnpic, ynpic, znpic):
     res = []
@@ -24,3 +26,25 @@ def generate(x0film, dxbrush, y0brush, dybrush, brushperiod, ymax, xnpic, ynpic,
         res.append('znpic_film = {0}'.format(xnpic))
         ydispl += brushperiod
     return "\n".join(res)
+
+
+def generate_horizontal(x0brush, dxbrush, y0brush, y1brush, brushperiod, xmax, xnpic, ynpic, znpic):
+    res = []
+    xdispl = 0.0
+    while xdispl + x0brush + dxbrush <= xmax:
+        res.append('film = on')
+        res.append('x0film = {0}'.format(xdispl + x0brush))
+        res.append('filmwidth = {0}'.format(dxbrush))
+        res.append('gradwidth = 0')
+        res.append('y0film = {0}'.format(y0brush))
+        res.append('y1film = {0}'.format(y1brush))
+        res.append('z0film = 0.5')
+        res.append('z1film = 10.5')
+        res.append('nfilm = 1 ne')
+        res.append('mcr = 2.0')
+        res.append('xnpic_film = {0}'.format(xnpic))
+        res.append('ynpic_film = {0}'.format(ynpic))
+        res.append('znpic_film = {0}'.format(xnpic))
+        xdispl += brushperiod
+    return "\n".join(res)
+
