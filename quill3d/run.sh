5ac20c4c25dfacc82b5c4d64f22c451625dc9f31
Anton Golovanov <agolovanov256@gmail.com>
2021-10-25 15:57:11 +0300

    Add backward compatibility to previous config path format

--- quill3d/run.sh
+++ quill3d/run.sh
@@ -3,13 +3,23 @@
 # we do not want the script to continue if anything fails
 set -e
 
+YELLOW='\033[0;33m'
+NOCOLOR='\033[0m'
+
+config_filepath="$1"
 if [ ! -f "$1" ]; then
-    echo "Config file $1 not found! Create it or copy from ../quill3d-conf/example"
-    exit 1
+    old_config_filepath=../quill3d-conf/quill.conf"$1"
+    if [ -f $old_config_filepath ]; then
+        echo -e "${YELLOW}Defaulting to config [$old_config_filepath]. Prefer using absolute paths instead! ${NOCOLOR}"
+        config_filepath=$old_config_filepath
+    else
+        echo "Config file $1 not found!"
+        exit 1
+    fi
 fi
 
 # Create data folder if not exist; copy config file into it
-folder=`./parse.py "$1" | grep -A2 data_folder | tail -n1`
+folder=`./parse.py "$config_filepath" | grep -A2 data_folder | tail -n1`
 if [[ -z "$folder" ]]
 then
     folder="results"
@@ -18,7 +28,7 @@ echo "Data folder: $folder"
 sleep 1.5  # so the user has time to see the data folder
 
 # Determine the number of threads specified in the config
-threads=`./parse.py "$1" | grep -A1 n_sr | tail -n1`
+threads=`./parse.py "$config_filepath" | grep -A1 n_sr | tail -n1`
 if [[ -z "$threads" ]]
 then
     threads=8
@@ -27,10 +37,10 @@ echo "Threads: $threads"
 
 mkdir -p $folder
 
-cp "$1" $folder/
+cp "$config_filepath" $folder/
 
 # 1. Parsing config file (parse.sh)
 # 2. Running quill with input from step 1
 # 3. Adding timestamps to the Quill's console output
 # 4. Duplicating output to a log file (quill_log.txt)
-mpirun -n $threads bash -c "stdbuf -o 0 ./parse.py \"$1\" | { ./quill; } 2>&1" | awk '{ print strftime("%Y-%m-%d %H:%M:%S\t"), $0; fflush(); }' | tee $folder/quill_log.txt
+mpirun -n $threads bash -c "stdbuf -o 0 ./parse.py \"$config_filepath\" | { ./quill; } 2>&1" | awk '{ print strftime("%Y-%m-%d %H:%M:%S\t"), $0; fflush(); }' | tee $folder/quill_log.txt
